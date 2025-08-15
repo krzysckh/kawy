b8b44e376c87d63e361e9e99237ee44bc54fe401
Krzysztof Michałczyk <kpm@krzysckh.org>
2025-08-15 10:16:15 +0200

    opninions in own file, ids by name

--- Makefile
+++ Makefile
@@ -3,3 +3,5 @@ OL=ol-small
 all: public/index.html
 public/index.html: build.scm
 	$(OL) -i third-party/robusta -r build.scm
+clean:
+	rm -f public/index.html
--- build.scm
+++ build.scm
@@ -25,58 +25,7 @@
     (put 'briki    "w tygielku")
     ))
 
-(define *opinions*
-  (list
-   (opinion on "HAYB SIE PRZELEWA Kwiat"
-     tint   is "#f0f"
-     rating is 10
-     image  is "HAYB.sieprzelewa.kwiat.jpg"
-     link   is "https://haybcoffee.pl/produkt/sie-przelewa-kwiat/"
-     brew   is drip
-     text   is ("genialna. "
-                "zdecydowany faworyt do wiosennego przelewu. "
-                "w smaku nierzadko herbaciana. "
-                "bardzo delikatna. przy parzeniu z większej ilości kawy zupełnie nie kwaśna, tylko lekuteńko gorzkawa. "
-                "zdecydowanie godna polecenia."))
-   (opinion on "HAYB SIE PRZELEWA Owoc"
-     tint   is "#ec6b20"
-     rating is 6
-     image  is "HAYB.sieprzelewa.owoc.jpg"
-     link   is "https://haybcoffee.pl/produkt/sie-przelewa-owoc/"
-     brew   is drip
-     text   is ("spoko, przyjemna, codzienna. lekko kwaskowa. "
-                "niestety cenowo taka sama jak kwiat, a — moim zdaniem — ma sporo uboższy smak, co odejmuje jej parę punktów"))
-   (opinion on "BONORA Καφές Ελληνικός"
-     tint   is "#919191"
-     rating is 7
-     image  is "BONORA.ellinikos.kafes.jpg"
-     link   is "https://www.sklavenitis.gr/eidi-proinoy-rofimata/kafedes-rofimata-afepsimata/kafedes-ellinikoi/bonora-ellinikos-kafes-487gr-240959/"
-     brew   is briki
-     text   is ("chyba moja ulubiona wśród kaw po grecku. "
-                "wprost z greckiego supermarketu. dużo lepsza od loumidis papagalos kupionej w kuchniach świata. "
-                "smakuje najlepiej zimą, z odrobiną kardamonu."))
-   (opinion on "JA-WA kolubmia bezkofeinowa"
-     tint   is "gold"
-     rating is 7
-     image  is "JAWA.decaf.kolumbia.jpg"
-     link   is "https://www.palarniakawyjawa.pl/kawa-bezkofeinowa-kolumbia/"
-     brew   is (moka espresso)
-     text   is ("naprawdę spoko. bezkofeinowość w smaku zdecydowanie niewyczuwalna. "
-                "kwaskowa w przyjemny sposób. palona ciemno."))
-   (opinion on "DIY Indie Cherry AA"
-     tint   is "#D9001D"
-     rating is 3
-     image  is ("DIY.cafemia.indiecherry1.jpg"
-                "DIY.cafemia.indiecherry2.jpg"
-                "DIY.cafemia.indiecherry3.jpg")
-     link   is "https://cafemia.pl/pl/p/INDIA-CHERRY-AA/82"
-     brew   is (moka espresso drip ew)
-     text   is ("zabawa przednia — wypalałem ją na kilka sposobów (m.in. na ognisku). "
-                "niestety wyniki (zupełnie nie zaskakująco) raczej słabe. "
-                "sporo wypaliłem zbyt ciemno, sporo zbyt jasno. bez sprzętu ciężko jest uzyskać jednolite wypalenie. "
-                "no nie była ona najlepsza ;). ")
-     )
-   ))
+,load "opinions.scm"
 
 (define (start-gensym)
   (thread
@@ -171,10 +120,12 @@
       ()
       (cons (f (car a) (car b)) (map2 f (cdr a) (cdr b)))))
 
+(define title->id (string->regex "s/[ ]+/./g"))
+
 (λ (_)
   (start-gensym)
   (let ((f (open-output-file "public/index.html"))
-        (ids (map (λ (x) (x)) (make-list (length *opinions*) gensym))))
+        (ids (map title->id (map (λ (o) (get o 'on #f)) *opinions*))))
     (write-bytes
      f
      (string->bytes
